006e0c999c6bd4179fa5a935b7e4532aa0980b26
Justin Brooks <justin@datatorch.io>
2020-06-03 23:00:35 -0400

    chore: updated context type

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version='0.0.1',
     description='A CLI and library for interacting with DataTorch',
     author='DataTorch',
-    author_email='justin@datatorch.io',
+    author_email='support@datatorch.io',
     entry_points={
         'console_scripts': [
             'datatorch=datatorch.cli:main',
@@ -20,8 +20,11 @@ setup(
         ]
     },
     url='https://github.com/datatorch/python',
+    packages=['datatorch'],
     long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requirements=requirements,
+    python_requires='>=3.6',
     license='MIT license',
     zip_safe=False
 )
