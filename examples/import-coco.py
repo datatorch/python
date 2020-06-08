b2cbb777a3563ecea16f6118c79d40f03037989b
Justin Brooks <justin@datatorch.io>
2020-06-08 12:09:26 -0400

    chore: formatting

--- examples/import-coco.py
+++ examples/import-coco.py
@@ -1,15 +1,21 @@
-
-from datatorch.api import ApiClient, Where, Label, Annotation, Segmentations, BoundingBox
+from datatorch.api import (
+    ApiClient,
+    Where,
+    Label,
+    Annotation,
+    Segmentations,
+    BoundingBox,
+)
 
 from pycocotools.coco import COCO
 
 
 if __name__ == "__main__":
-    
+
     # DataTorch project ID
-    project_id = 'DATATORCH_PROJECT_ID'
+    project_id = "DATATORCH_PROJECT_ID"
     # Path to annotation file
-    anno_file = 'path/to/coco'
+    anno_file = "path/to/coco"
     # Only add annotations above this score
     min_source = 0.8
 
@@ -18,12 +24,11 @@ if __name__ == "__main__":
     project = api.project(project_id)
     labels = project.labels()
 
-    print('=' * 50)
-    print(f'Project: {project.name}')
+    print("=" * 50)
+    print(f"Project: {project.name}")
     names = [label.name for label in labels]
     print(f'Labels: {" ".join(names)}')
-    print('=' * 50)
-
+    print("=" * 50)
 
     def category_in_project(name: str) -> Label:
         """ Returns category in project """
@@ -32,67 +37,61 @@ if __name__ == "__main__":
                 return cat
         return None
 
-
     # Load coco
     coco = COCO(anno_file)
     cats = coco.loadCats(coco.getCatIds())
-    names = [cat['name'] for cat in cats]
+    names = [cat["name"] for cat in cats]
 
     label_maping = {}
     for cat in cats:
-        name = cat['name']
+        name = cat["name"]
         found = category_in_project(name)
 
         if not found:
             print(f'label "{name}" not found in project')
         else:
-            label_maping[cat['id']] = found
-        
+            label_maping[cat["id"]] = found
 
     print(f'COCO Categories: {" ".join(names)}')
 
     for img_id in coco.getImgIds():
-        img, = coco.loadImgs(img_id)
-        name = img['file_name']
+        (img,) = coco.loadImgs(img_id)
+        name = img["file_name"]
 
-        find_by_name = Where(name=name, mimetype__starts_with='image')
+        find_by_name = Where(name=name, mimetype__starts_with="image")
         dt_files = project.files(find_by_name)
         files_count = len(dt_files)
 
         if files_count > 1:
-            print(f'\nMultiple files found with name {name}, skipping')
+            print(f"\nMultiple files found with name {name}, skipping")
             continue
-        
+
         if files_count == 0:
-            print(f'\nNo files found with name {name}, skipping')
+            print(f"\nNo files found with name {name}, skipping")
             continue
-        
-        print(f'\n{name} found. Importing annotations')
+
+        print(f"\n{name} found. Importing annotations")
         dt_file = dt_files[0]
 
         # load file annotations
-        anno_ids = coco.getAnnIds(imgIds=img['id'])
+        anno_ids = coco.getAnnIds(imgIds=img["id"])
         annos = coco.loadAnns(anno_ids)
 
         for anno in annos:
             # Create annotation
-            if anno.get('datatorch_id') is not None:
+            if anno.get("datatorch_id") is not None:
                 print(f'Annotation {anno["id"]} already exists in DataTorch, skipping')
-            
-            source = anno.get('source')
-            if anno.get('source') is not None and source < min_source:
+
+            source = anno.get("source")
+            if anno.get("source") is not None and source < min_source:
                 continue
 
-            label = label_maping[anno['category_id']]
+            label = label_maping[anno["category_id"]]
             if label is None:
                 continue
-            
+
             dt_anno = Annotation(label=label)
-            bbox = BoundingBox.xywh(*anno['bbox'])
+            bbox = BoundingBox.xywh(*anno["bbox"])
             dt_anno.add(bbox)
             print(bbox.__dict__)
             dt_file.add(dt_anno)
-            
-
-    
-
