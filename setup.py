d9535369d8c5a8ff45bd7423a0b9f9f3884b0386
Justin Brooks <justin@datatorch.io>
2020-06-11 12:45:36 -0400

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements_agents = ["psutil~=5.0"]
 
 setup(
     name="datatorch",
-    version="0.1.1",
+    version="0.1.2",
     description="A CLI and library for interacting with DataTorch",
     author="DataTorch",
     author_email="support@datatorch.io",
