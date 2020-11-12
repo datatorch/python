4e3bff6c8e093ef6d9512a445c3152ffae959b39
Justin Brooks <justin@datatorch.io>
2020-11-12 14:27:14 -0500

    chore(release): v0.3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements_agents = []
 
 setup(
     name="datatorch",
-    version="0.2.6",
+    version="0.3.0",
     description="A CLI and library for interacting with DataTorch",
     author="DataTorch",
     author_email="support@datatorch.io",
